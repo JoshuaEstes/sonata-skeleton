d085df6f8ae6ab1f1a11c81abfc96ba474f9e4a3
Joshua Estes <Joshua.Estes@ScenicCityLabs.com>
2014-03-24 13:31:06 -0500

    Bug fix and working on manifest

---
+++ app/Resources/puppet/manifests/base.pp
@@ -0,0 +1,23 @@
+####
+#
+# Really bad puppet manifest file, this is just
+# to get things up and working, not to be used
+# in production
+#
+$packages = [
+    'php5',
+    'php-apc',
+    'php5-curl',
+    'php5-intl',
+    'php5-mcrypt',
+    'php5-sqlite',
+    'php5-xdebug',
+    'nginx',
+    'mysql-client',
+    'mysql-server',
+    'php5-mysql',
+]
+
+package { $packages:
+    ensure => latest,
+}
